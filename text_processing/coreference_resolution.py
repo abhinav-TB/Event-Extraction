78588fe9ea2d6e04016598977e669f110fb4b3be
Fazil Babu <fazil47babu@gmail.com>
2022-06-30 13:48:07 +0000

    Added publised date to csv

--- text_processing/coreference_resolution.py
+++ text_processing/coreference_resolution.py
@@ -4,12 +4,15 @@ sys.path.append("/workspaces/Event-Extraction")
 from allennlp.predictors.predictor import Predictor
 from mongodb.config import db
 from nltk.tokenize import sent_tokenize
-import csv
+from csv import DictWriter
 
 collection = db["kashmir_news"]
 
 csvfile = open("outputs/events.csv", "w")
-csvwriter = csv.writer(csvfile)
+csvwriter = DictWriter(
+    csvfile, fieldnames=["article_id", "article_title", "article_date", "event"]
+)
+csvwriter.writeheader()
 length = collection.count_documents({})
 model_url = "https://storage.googleapis.com/allennlp-public-models/coref-spanbert-large-2020.02.27.tar.gz"
 predictor = Predictor.from_path(model_url)
@@ -19,13 +22,21 @@ for i, document in enumerate(collection.find()):
     try:
         title = document["title"]
         description = document["description"]
+        article_date = document["published_at"]
         prediction = predictor.predict(document=description)
         new_description = predictor.coref_resolved(description)
 
         sentences = sent_tokenize(new_description)
 
         for sentence in sentences:
-            csvwriter.writerow([i, title, sentence])
+            csvwriter.writerow(
+                {
+                    "article_id": i,
+                    "article_title": title,
+                    "article_date": article_date,
+                    "event": sentence,
+                }
+            )
 
     except Exception as e:
         print("error in writing sentence: {}".format(e))
