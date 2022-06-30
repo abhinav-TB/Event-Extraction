7e1fbecd719a6b27ead831f19373596bce0b1527
Fazil Babu <fazil47babu@gmail.com>
2022-06-30 12:13:34 +0000

    Article number is now a part of each csv row

--- text_processing/coreference_resolution.py
+++ text_processing/coreference_resolution.py
@@ -1,36 +1,31 @@
 import sys
-sys.path.append('/workspaces/Event-Extraction')
+
+sys.path.append("/workspaces/Event-Extraction")
 from allennlp.predictors.predictor import Predictor
 from mongodb.config import db
 from nltk.tokenize import sent_tokenize
 import csv
 
-collection = db['kashmir_news']
+collection = db["kashmir_news"]
 
-csvfile = open('outputs/event.csv', 'w')
-csvwriter = csv.writer(csvfile) 
+csvfile = open("outputs/events.csv", "w")
+csvwriter = csv.writer(csvfile)
 length = collection.count_documents({})
 model_url = "https://storage.googleapis.com/allennlp-public-models/coref-spanbert-large-2020.02.27.tar.gz"
 predictor = Predictor.from_path(model_url)
 
-for i,document in enumerate(collection.find()):
-    print("----processing document {}/{}".format(i+1,length))
+for i, document in enumerate(collection.find()):
+    print("----processing document {}/{}".format(i + 1, length))
     try:
-        description = document['description']
+        description = document["description"]
         prediction = predictor.predict(document=description)
         new_description = predictor.coref_resolved(description)
 
         sentences = sent_tokenize(new_description)
-    
+
         for sentence in sentences:
-                csvwriter.writerow([sentence])
+            csvwriter.writerow([sentence, i])
 
     except Exception as e:
         print("error in writing sentence: {}".format(e))
 csvfile.close()
-
-     
-
-
-
-
