d652c67ec2cd71bdf4c9680247c6c5ed00a0eb98
Fazil Babu <fazil47babu@gmail.com>
2022-07-01 07:00:38 +0000

    Simple location and date tagging done

--- text_processing/location_and_time.py
+++ text_processing/location_and_time.py
@@ -1,30 +1,60 @@
-from spacy import load
-from csv import DictReader
+from csv import DictReader, DictWriter
+from allennlp_models import pretrained
 
-nlp = load("en_core_web_sm")
+ner_predictor = pretrained.load_predictor("tagging-fine-grained-transformer-crf-tagger")
+
+csvfile = open("outputs/tagged_events_raw.csv", "w")
+csvwriter = DictWriter(
+    csvfile,
+    fieldnames=[
+        "article_id",
+        "article_title",
+        "article_date",
+        "event",
+        "location_prediction",
+        "date_prediction",
+    ],
+)
+csvwriter.writeheader()
 
 with open("outputs/events.csv", "r") as f:
     reader = DictReader(f)
-    for row in reader:
+    last_word_was_date = False
+    last_word_was_location = False
+    for i, row in enumerate(reader):
         event = row["event"]
-        doc = nlp(event)
-        for ent in doc.ents:
-            if ent.label_ == "DATE":
-                print(ent.text)
-        
-
-
-# Process whole documents
-text = (
-    "When Sebastian Thrun started working on self-driving cars at "
-    "Google in 2007, few people outside of the company took him "
-    "seriously. “I can tell you very senior CEOs of major American "
-    "car companies would shake my hand and turn away because I wasn’t "
-    "worth talking to,” said Thrun, in an interview with Recode earlier "
-    "this week."
-)
-doc = nlp(text)
-
-for ent in doc.ents:
-    if ent.label_ == "DATE":
-        print(ent.text)
+        prediction = ner_predictor.predict(sentence=event)
+        words = prediction["words"]
+        tags = prediction["tags"]
+        date = ""
+        location = ""
+        for word, tag in zip(words, tags):
+            if tag != "O":
+                if tag.split("-")[1] == "DATE":
+                    if last_word_was_date:
+                        date += f" {word}"
+                    else:
+                        date = word
+                    last_word_was_date = True
+                elif tag.split("-")[1] == "GPE":
+                    if last_word_was_location:
+                        location += f" {word}"
+                    else:
+                        location = word
+                    last_word_was_location = True
+                else:
+                    last_word_was_date = False
+                    last_word_was_location = False
+            else:
+                last_word_was_date = False
+                last_word_was_location = False
+        csvwriter.writerow(
+            {
+                "article_id": row["article_id"],
+                "article_title": row["article_title"],
+                "article_date": row["article_date"],
+                "event": row["event"],
+                "location_prediction": location,
+                "date_prediction": date,
+            }
+        )
