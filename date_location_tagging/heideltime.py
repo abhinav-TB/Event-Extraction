55bb02c03a6ccf8d98fec4c9b0f9886625872fc8
Fazil Babu <fazil47babu@gmail.com>
2022-08-18 16:07:46 +0000

    Ran heideltime algo on new data

--- date_location_tagging/heideltime.py
+++ date_location_tagging/heideltime.py
@@ -1,16 +1,23 @@
 from py_heideltime import py_heideltime
 from csv import DictWriter, DictReader
+
 csvfile = open("../outputs/heidal_Time.csv", "w")
 csvwriter = DictWriter(
     csvfile, fieldnames=["article_id", "article_title", "date_prediction", "event"]
 )
 csvwriter.writeheader()
 # read from a csvfile
-with open('../outputs/events.csv', 'r') as f:
+with open("../outputs/events.csv", "r") as f:
     reader = DictReader(f)
-    for i,row in enumerate(reader):
+    for i, row in enumerate(reader):
         print("----processing document {}".format(i + 1))
-        results =  py_heideltime(row['event'], language='english', date_granularity="day", document_type='news', document_creation_time= row['article_date'][:10])
+        results = py_heideltime(
+            row["event"],
+            language="english",
+            date_granularity="day",
+            document_type="news",
+            document_creation_time=row["article_date"][:10],
+        )
         csvwriter.writerow(
             {
                 "article_id": row["article_id"],
@@ -18,5 +25,4 @@ with open('../outputs/events.csv', 'r') as f:
                 "event": row["event"],
                 "date_prediction": results[0],
             }
-            
         )
